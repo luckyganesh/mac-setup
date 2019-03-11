94e73c1073a57df5bba3f6c29a4dda0d15a8025a
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-03-11 18:21:11 +0530

    Created scripts and setup.sh.

---
+++ scripts/applications.sh
@@ -0,0 +1,22 @@
+#### APPLICATIONS ####
+
+
+#installing google-chrome
+echo "Installing chrome";
+brew cask install google-chrome
+
+#installing iterm2
+echo "Installing iterm2";
+brew cask install iterm2
+
+#installing vlc
+echo "Installing vlc";
+brew cask install vlc
+
+#installing vscode
+echo "Installing visual-studio-code";
+brew cask install visual-studio-code
+
+#installing slack
+echo "Installing slack"
+brew cask install slack
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+echo "APPLICATIONS";
+sh ./scripts/applications.sh
+
