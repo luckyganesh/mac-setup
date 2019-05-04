b19053b6474ef864289d3d449d8fff95a15eb08e
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-05-04 15:02:59 +0530

    added installing zsh-syntax-highlighting

--- scripts/zshFeatures.sh
+++ scripts/zshFeatures.sh
@@ -1,2 +1,5 @@
 echo "Installing zsh-autoSuggestions";
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions;
+
+echo "Installing zsh-syntax-highlighting";
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
