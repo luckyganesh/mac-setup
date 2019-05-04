acaacb026f67f89dfdceaf11e3c142bca9e06a2d
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-05-04 14:19:16 +0530

    added installing vscode plugins

---
+++ scripts/vscode_plugins.sh
@@ -0,0 +1,11 @@
+# installing command_line_lanchure
+ln -s /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code /usr/local/bin/code
+
+code --install-extension peterjausovec.vscode-docker
+code --install-extension tootone.org-mode
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-mssql.mssql
+code --install-extension patbenatar.advanced-new-file
+code --install-extension coenraads.bracket-pair-colorizer
+code --install-extension azemoh.one-monokai
+code --install-extension formulahendry.auto-rename-tag
