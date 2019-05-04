df562ebd33e11a96c85d659daba32df973bcace2
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-05-04 14:36:20 +0530

    used the power of arrays for installing vscode-plugins

--- scripts/vscode_plugins.sh
+++ scripts/vscode_plugins.sh
@@ -1,11 +1,15 @@
 # installing command_line_lanchure
 ln -s /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code /usr/local/bin/code
 
-code --install-extension peterjausovec.vscode-docker
-code --install-extension tootone.org-mode
-code --install-extension esbenp.prettier-vscode
-code --install-extension ms-mssql.mssql
-code --install-extension patbenatar.advanced-new-file
-code --install-extension coenraads.bracket-pair-colorizer
-code --install-extension azemoh.one-monokai
-code --install-extension formulahendry.auto-rename-tag
+vscode_plugins=(
+    peterjausovec.vscode-docker
+    tootone.org-mode
+    esbenp.prettier-vscode
+    ms-mssql.mssql
+    patbenatar.advanced-new-file
+    coenraads.bracket-pair-colorizer
+    azemoh.one-monokai
+    formulahendry.auto-rename-tag
+)
+
+for i in "${vscode_plugins[@]}"; do code --install-extension "$i"; done
