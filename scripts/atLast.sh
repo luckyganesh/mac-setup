16bf8eb9b58bd54ca57770be21578cbb1bee92b2
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-05-04 14:44:55 +0530

    Added installing mysql

--- scripts/atLast.sh
+++ scripts/atLast.sh
@@ -6,3 +6,6 @@ open https://download.oracle.com/otn/java/jdk/11.0.3+12/37f5e150db5247ab9333b11c
 
 # installing docker
 https://download.docker.com/mac/stable/Docker.dmg
+
+# installing mysql
+https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-8.0.16-macos10.14-x86_64.dmg
