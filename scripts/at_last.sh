a61c183fd1a529b53688490f5bd20b9c86b68639
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-05-19 11:11:24 +0530

    updated atLast.sh

--- scripts/at_last.sh
+++ scripts/at_last.sh
@@ -5,7 +5,7 @@ open https://www.jetbrains.com/idea/download/download-thanks.html?platform=mac
 open https://download.oracle.com/otn/java/jdk/11.0.3+12/37f5e150db5247ab9333b11c1dddcd30/jdk-11.0.3_osx-x64_bin.dmg
 
 # installing docker
-https://download.docker.com/mac/stable/Docker.dmg
+open https://download.docker.com/mac/stable/Docker.dmg
 
 # installing mysql
-https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-8.0.16-macos10.14-x86_64.dmg
+open https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-8.0.16-macos10.14-x86_64.dmg
